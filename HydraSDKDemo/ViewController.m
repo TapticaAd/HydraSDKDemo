2f227c54d0cf39a8c4fe49d64d618effdf5ed38f
nissopa <nissim.p@taptica.com>
2015-04-29 15:36:35 +0300

    Updated the HydraSDK

--- HydraSDKDemo/ViewController.m
+++ HydraSDKDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <HydraSDK/HydraSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <HAdBannerDelegate, HInterstitialDelegate>{
+    HAdBanner *banner;
+    HInterstitial *interstitial;
+    __weak IBOutlet UIView *bannerHolderView;
+}
 
 @end
 
@@ -19,9 +24,33 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (!banner) {
+        banner = [[HAdBanner alloc] initWithAdSize:(CGSize){320, 50} origin:CGPointZero];
+        banner.unitAdID = @"115";
+        banner.delegate = self;
+        banner.rootViewController = self;
+        banner.requestParams.shouldOpenInNativeBrowser = YES;
+        [banner loadAd];
+        [bannerHolderView addSubview:banner];
+    }
+}
+- (IBAction)presentInterstitial:(UIButton *)sender {
+    interstitial = [[HInterstitial alloc] init];
+    interstitial.unitAdID = @"113";
+    interstitial.delegate = self;
+    [interstitial loadAd];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark HInterstitialDelegate
+- (void)interstitialDidReceiveAd:(HInterstitial *)ad {
+    [ad presentFromRootViewController:self];
 }
 
 @end
